06dac4100659e3b8d40a6784a14b3b5f4353510d
Andy <3775@holbertonschool.com>
2022-02-04 19:46:19 +0000

    Add task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - print numbers 0-9
+ *
+ *Return: always return 0
+ */
+int main(void)
+{
+	int num = 48;
+
+	while (num < 58)
+		{
+		putchar(num);
+		num++;
+		}
+	printf("\n");
+	return (0);
+}
