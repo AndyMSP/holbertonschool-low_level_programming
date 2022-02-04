59773d440c1c3e2c0f7205dfd63d646bd60570c9
Andy <3775@holbertonschool.com>
2022-02-04 19:16:35 +0000

    Add task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - print numbers 0-9
+ *
+ *Return: always return 0
+ */
+int main(void)
+{
+	int num;	
+
+	while (num < 10)
+		{
+		printf("%i",num);
+		num++;
+		}
+	printf("\n");
+	return (0);
+}
