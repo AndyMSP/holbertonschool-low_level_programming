f0df668ef3f774a8598976251d8838699f3faf22
Andy <3775@holbertonschool.com>
2022-02-08 19:25:31 +0000

    Try task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet 10 times
+ *
+ * Return: No return value
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	
+
+	for (i = 0; i <= 9; i++)
+	{
+		print_alphabet();
+	}
+}
