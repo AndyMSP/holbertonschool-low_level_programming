83e5c1ecf7d2d311c405308a8a3256fb6fd53e1f
Andy <3775@holbertonschool.com>
2022-02-08 23:24:01 +0000

    Correct task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,24 +11,10 @@ int print_last_digit(int n)
 {
 	int last;
 
-	last = _abs(n) % 10;
+	if (n < 0)
+		last = (-1 * n) % 10;
+	else
+		last = n % 10;
 	_putchar (last + '0');
 	return (last);
 }
-
-/**
- * _abs - returns absolute value of input argument
- * @n: any integer
- *
- * Return: absolute value of input
- */
-int _abs(int n)
-{
-	if (n == 0)
-		return (0);
-	else if (n < 0)
-		return (n * -1);
-	else
-		return (n);
-}
-
