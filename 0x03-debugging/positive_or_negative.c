4895e91c873e37454eb113fc9af71bc588bab468
Andy <3775@holbertonschool.com>
2022-02-10 17:35:36 +0000

    Add task 0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ *positive_or_negative - prints a random number and describes it as positive, negative or 0
+ *
+ *Return: Always return 0
+ */
+int positive_or_negative(void)
+{
+	int n;
+
+	srand(time(0));
+	n = 0;
+	/* your code goes there */
+	printf("%i is ", n);
+	if (n < 0)
+		printf("negative\n");
+	else if (n == 0)
+		printf("zero\n");
+	else
+		printf("positive\n");
+	return (0);
+}
