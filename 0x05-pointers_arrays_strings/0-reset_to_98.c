254a2a0992f7655e1d6ff5e0d1c945e119191d38
Andy <3775@holbertonschool.com>
2022-02-14 19:02:53 +0000

    Add task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - assign value of 98 to address pointed to
+ * @n: integer that is being pointed to
+ *
+ * Return: No return value
+ */
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+
+}
