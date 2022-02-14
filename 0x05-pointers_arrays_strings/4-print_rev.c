13f84ac8ebe89a88692c1c8b0aec4862e095771e
Andy <3775@holbertonschool.com>
2022-02-14 21:25:55 +0000

    Add task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,14 +1,43 @@
 #include "main.h"
-#include <stdio.h>
+
+int len(char *s);
 
 /**
-* print_rev - prints argument string in reverse
-* @str: pointer to a string
-*
-* Return: No return value
-*/
+ * print_rev - prints argument string in reverse
+ * @str: pointer to a string
+ *
+ * Return: No return value
+ */
 
 void print_rev(char *str)
 {
-	printf("%lu\n", sizeof(str[]));
+	int i;
+
+	for (i = len(str); i >= 0; i--)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
+
+/**
+ * len - Calculates length of a string
+ * @s: a string ending with '\0'
+ *
+ * Return: int length of string
+ */
+
+int len(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		i++;
+	}
+
+	return (i);
+
 }
