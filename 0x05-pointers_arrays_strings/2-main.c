7b3cfcc3c5171432643b9550d08231133f4280f7
Andy <3775@holbertonschool.com>
2022-02-14 20:31:50 +0000

    Add task 3

--- 0x05-pointers_arrays_strings/2-main.c
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -11,7 +11,7 @@ int main(void)
 	char *str;
 	int len;
 
-	str = "one";
+	str = "";
 	len = _strlen(str);
 	printf("%d\n", len);
 	return (0);
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string to standard output
+ * @str: a string
+ *
+ * Return: No return value
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while(str[i])
+	{
+		_putchar(str[i]);
+		i++;	
+	}
+
+	_putchar('\n');
+}
