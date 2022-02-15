79a51ac41b9c47e186809ad009ebf3b50b787d3a
Andy <3775@holbertonschool.com>
2022-02-15 22:23:28 +0000

    Correct task 8

--- 0x05-pointers_arrays_strings/5-main.c
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -1,17 +1,20 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
+
+void print_array(int *, int);
 
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char s[9] = "abcdefgh";
+	int array[1] = {
+		98
+	};
 
-	printf("%s\n", s);
-	rev_string(s);
-	printf("%s\n", s);
+	print_array(array, 0);
+	print_array(array, -1);
 	return (0);
 }
