35575cc9ebe5e979b5fd8e998799c928e3d11ae8
Andy <3775@holbertonschool.com>
2022-02-18 15:29:07 +0000

    Add task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalize first letter of each word
+ * @str: pointer to a string
+ *
+ * Return: pointer to string
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' ||
+			str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' ||
+			str[i] == '"' || str[i] == 40 || str[i] == 41 || str[i] == '}' ||
+			str[i] == '}')
+		{
+			i++;
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] = str[i] - 32;
+		}
+		else
+			i++;
+	}
+
+	return (str);
+}
