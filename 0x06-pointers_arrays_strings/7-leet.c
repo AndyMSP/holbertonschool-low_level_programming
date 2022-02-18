817cae47e472c2f59295c81bf39f987a1041cb3d
Andy <3775@holbertonschool.com>
2022-02-18 17:36:57 +0000

    Push task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * leet - convert string into 1337
+ * @s: string
+ *
+ * Return: pointer to string
+ */
+
+char *leet(char *s)
+{
+	int i = 0;
+	int j = 0;
+	char rep[] = "Aa44Ee33Oo00Tt77Ll11";
+
+	while (s[i])
+	{
+		if (s[i] == 'A' || s[i] == 'a' || s[i] == 'E' || s[i] == 'e' ||
+			s[i] == 'O' || s[i] == 'o' || s[i] == 'T' || s[i] == 't' ||
+			s[i] == 'L' || s[i] == 'l')
+		{
+			j = 0;
+			while (s[i] != rep[j] && j < 15)
+			{
+				j++;
+			}
+			s[i] = rep[j + 2];
+		}
+
+	i++;
+	}
+	return (s);
+}
