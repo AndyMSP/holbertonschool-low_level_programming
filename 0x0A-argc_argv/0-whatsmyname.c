cb0597d8c36caa6aad58620e97f72db4de8d6789
Andy <3775@holbertonschool.com>
2022-02-27 21:55:30 +0000

    Add task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments passed to the program
+ * @argv: pointer to pointer to char
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
