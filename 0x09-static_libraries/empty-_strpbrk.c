8326c8b1126fb858a8a4bd68cd2a2a5150da763e
Andy <3775@holbertonschool.com>
2022-02-27 01:47:09 +0000

    Debug

--- 0x09-static_libraries/empty-_strpbrk.c
+++ 0x09-static_libraries/empty-_strpbrk.c
@@ -1,5 +1,11 @@
-/*
- * Empty file to satisfy library
+/**
+ * _strpbrk - empty function
+ * @s: pointer to character
+ * @accept: pointer to character
+ *
+ * Return: pointer to a character
  */
+char *_strpbrk(char *s, char *accept)
+{
 
-char *_strpbrk(char *s, char *accept);
+}
--- 0x09-static_libraries/empty-_strstr.c
+++ 0x09-static_libraries/empty-_strstr.c
@@ -1,5 +1,12 @@
-/*
- * Empty file to satisfy library
+/**
+ * _strstr - empty function
+ * @haystack: pointer to character
+ * @needle: pointer to character
+ *
+ * Return: char
  */
 
-char *_strstr(char *haystack, char *needle);
+char *_strstr(char *haystack, char *needle)
+{
+
+}
