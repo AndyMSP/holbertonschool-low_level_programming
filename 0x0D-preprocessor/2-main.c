28346f17041d18b0ab31cf9b4af4b0fc14decbee
Andy <3775@holbertonschool.com>
2022-03-08 12:38:50 +0000

    Add task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of file that the executable was compiled
+ *
+ * Return: no return value
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
