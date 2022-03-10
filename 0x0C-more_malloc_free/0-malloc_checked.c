afa530402d2f12c5cc35b361e8997de8ede47562
Andy <3775@holbertonschool.com>
2022-03-10 19:39:42 +0000

    Add task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * malloc_checked - Returns a pointer to newly allocated memory or upon
+ * if malloc fails cause normal process termination with a status value
+ * of 98
+ * @b: an unsigned int
+ *
+ * Return: pointer to allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+
+	return ptr;
+}
