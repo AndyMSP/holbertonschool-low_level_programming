9ed9e65b51d810a2433393e2018f5d2a07b4a30c
Andy <3775@holbertonschool.com>
2022-03-11 00:18:55 +0000

    Add task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 
+
 /**
  * struct dog - contains name, age and owner
  * @name: pointer to a char
@@ -14,13 +15,14 @@ struct dog
 	char *owner;
 };
 
+
+typedef struct dog dog_t;
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void print_dog(struct dog *d);
-
-/*
- *	dog_t *new_dog(char *name, float age, char *owner);
- *	void free_dog(dog_t *d);
- */
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
