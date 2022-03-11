1589ffe467a75b525f3d98f6c273271a0355d7fb
Andy <3775@holbertonschool.com>
2022-03-11 04:41:38 +0000

    Debug

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,15 +13,20 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-
 	dog_t ndog, *ptr;
+	char *name_copy, *owner_copy;
 
-	ndog.name = name;
+	name_copy = malloc(sizeof(char) * (strlen(name) + 1);
+	owner_copy = malloc(sizeof(char) * (strlen(owner) + 1);
+
+	strcpy(name_copy, name);
+	strcpy(owner_copy, owner);
+
+	ndog.name = name_copy;
 	ndog.age = age;
-	ndog.owner = owner;
+	ndog.owner = owner_copy;
 
 	ptr = &ndog;
 
 	return (ptr);
-	
 }
