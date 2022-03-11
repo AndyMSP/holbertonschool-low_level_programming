da4c5bb401a64fa5275a1dbf1a689a6b9263a805
Andy <3775@holbertonschool.com>
2022-03-11 01:15:20 +0000

    Debug

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,27 @@
 #include <stdlib.h>
 #include "dog.h"
 
+
+/**
+ * init_dog - initialize a variable of type struct dog
+ * @d: pointer to a struct dog
+ * @name: pointer to char
+ * @age: floating point integer
+ * @owner: pointer to char
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
+
+
 /**
  * new_dog - creates a new dog struct
  * name: pointer to char
@@ -21,11 +42,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 	ptr = &dog;
 
 /*	ptr = malloc(sizeof(dog));*/
-
+/*
 	printf("Name: %s\n", ptr->name);
 	printf("Age: %f\n", ptr->age);
 	printf("Owner: %s\n", ptr->owner);
-
+*/
 	return (ptr);
 
 }
