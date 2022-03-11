484c3a66ce47bcb99da9123a3cb5e216c330388e
Andy <3775@holbertonschool.com>
2022-03-11 04:19:56 +0000

    Debug

--- 0x0E-structures_typedef/4-main.c
+++ 0x0E-structures_typedef/4-main.c
@@ -10,14 +10,7 @@ int main(void)
 {
 	dog_t *my_dog;
 
-	my_dog = new_dog("Andy", 3.5, "Bob");
-
-	printf("Name: %s + 1\n", my_dog->name);
-/*	printf("Age: %f\n", my_dog->age);
-	printf("Owner: %s\n", my_dog->owner);
-*/
-	printf("Name again: %s\n", my_dog->name);
-
+	my_dog = new_dog("Poppy", 3.5, "Bob");
 	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
 	return (0);
 }
--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,52 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include "dog.h"
 
-
 /**
- * init_dog - initialize a variable of type struct dog
- * @d: pointer to a struct dog
+ * new_dog - create new dog struct and pointer to it
  * @name: pointer to char
  * @age: floating point integer
  * @owner: pointer to char
- */
-
-void init_dog(struct dog *d, char *name, float age, char *owner)
-{
-
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
-}
-
-
-/**
- * new_dog - creates a new dog struct
- * name: pointer to char
- * age: float
- * owner: pointer to char
  *
- * @Return: pointer to new dog
+ * Return: pointer to initialized dog struct
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 
-	dog_t dog, *ptr;
+	dog_t ndog, *ptr;
 
-	init_dog(&dog, name, age, owner);
+	ndog.name = name;
+	ndog.age = age;
+	ndog.owner = owner;
 
-	ptr = &dog;
+	ptr = &ndog;
 
-/*	ptr = malloc(sizeof(dog));*/
-/*
-	printf("Name: %s\n", ptr->name);
-	printf("Age: %f\n", ptr->age);
-	printf("Owner: %s\n", ptr->owner);
-*/
 	return (ptr);
-
+	
 }
