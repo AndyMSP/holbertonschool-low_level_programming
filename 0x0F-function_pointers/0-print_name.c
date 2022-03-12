ee3c267b12cb3f8a384d60a6288851ed0ee7ff0c
Andy <3775@holbertonschool.com>
2022-03-12 23:11:44 +0000

    add task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name using function pointers to functions
+ * which print strings in different styles
+ * @name: pointer to char
+ * @f: pointer to function that returns void and takes pointer
+ * to char as parameter
+ *
+ * Return: No return value
+ */
+
+void print_name(char *name, void(*f)(char *))
+{
+	f(name);
+}
