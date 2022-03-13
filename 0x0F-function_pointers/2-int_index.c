f3bfed73004b79939be7038f59ab7f86f2862117
Andy <3775@holbertonschool.com>
2022-03-13 01:24:26 +0000

    Add task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - finds index of array where a match is made
+ * @array: an array of integers
+ * @size: number of elements in array
+ * @cmp: pointer to function that takes an int as an argument and
+ * returns and int
+ *
+ * Return: an integer representing the element in array where match is found
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+		return (-1);
+
+	while (i < size)
+	{
+		if (cmp(array[i]))
+			return (i);
+
+		i++;
+	}
+
+	return (-1);
+}
