af1ad15db313fe69c31f13b2c3df0d614b469022
Andy <3775@holbertonschool.com>
2022-03-23 01:24:44 +0000

    Add task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint - print value of n for all elements of a listint_t list
+ * @h: pointer to first node in list
+ *
+ * Return: number of nodes in list
+ */
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *node;
+	int count = 0;
+
+	node = h;
+
+	while(node != NULL)
+	{
+		printf("%i\n", node->n);
+		node = node->next;
+		count++;
+	}
+
+	return (count);
+}
