e9f92b651ceb2b6aa3472b41bcab937aa0cd1a90
Andy <3775@holbertonschool.com>
2022-04-04 03:25:04 +0000

    Debug

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,19 +9,32 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
 	char *buff;
-	size_t actual;
+	int fd, actual_read, actual_written;
 
+	/*open file and assign it to fd and check for errors*/
+	if (filename == NULL)
+		return (0);
 	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
+	/*designate required space for buffer and check for errors*/
 	buff = malloc(letters * sizeof(char));
+	if (buff == NULL)
+		return (0);
 
-	actual = read(fd, buff, letters);
-
-	actual = write(STDOUT_FILENO, buff, actual);
+	/*read data and write it to screen and check for errors*/
+	actual_read = read(fd, buff, letters);
+	if (actual_read == -1)
+		return (0);
+	actual_written = write(STDOUT_FILENO, buff, actual_read);
+	if (actual_written == -1)
+		return (0);
 
 	close(fd);
 
-	return (actual);
+	free(buff);
+
+	return (actual_written);
 }
