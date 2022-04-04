10710a839ca3bf0704ca2e55f459f616c4568ebc
Andy <3775@holbertonschool.com>
2022-04-04 04:49:56 +0000

    Debug

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,11 +18,11 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
-
-	printf("mode = %d\n", S_IRUSR | S_IWUSR);
-	printf("S_IRUSR = %d\n", S_IRUSR);
-	printf("S_IWUSR = %d\n", S_IWUSR);
-
+/*
+ *	printf("mode = %d\n", S_IRUSR | S_IWUSR);
+ *	printf("S_IRUSR = %d\n", S_IRUSR);
+ *	printf("S_IWUSR = %d\n", S_IWUSR);
+ */
 	/*write contents to file*/
 	if (text_content != NULL)
 	{
