dacbf99fd35eeb5075c113b7ddf05c6b5e9febe4
Andy <3775@holbertonschool.com>
2022-06-12 03:06:14 +0000

    Debug

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,8 +13,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
 	ht->size = size;
 	ht->array = malloc(size * sizeof(hn));
+	if (ht->array == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 	{
