6d8e1a07801622f0675170825d34497c451ef0b7
Andy <3775@holbertonschool.com>
2022-06-12 01:09:46 +0000

    Debug

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,10 +9,18 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
+	unsigned long int i;
 
 	ht = malloc(size * sizeof(hash_node_t));
 	if (ht == NULL)
 		return (NULL);
+
+	ht->size = size;
+
+	for (i = 0; i < size; i++)
+	{
+		ht->array = NULL;
+	}
 
 	return (ht);
 
