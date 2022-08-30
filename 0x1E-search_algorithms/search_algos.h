9a92ea4a693eec73e0a15a3953570d39d6df29d6
Andy <3775@holbertonschool.com>
2022-08-30 19:29:34 -0400

    Add task 1

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,5 +4,9 @@
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int rec_bin_search(int *array, int low_idx, int high_idx, int value);
+void print_array(int *array, int low_idx, int high_idx);
+
 
 #endif
