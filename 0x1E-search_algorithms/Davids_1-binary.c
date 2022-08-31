b6233a90ac4baa7cd9af9b5b1d9c933881619f62
Andy <3775@holbertonschool.com>
2022-08-31 13:39:31 -0400

    Get rid of David's

---
+++ 0x1E-search_algorithms/Davids_1-binary.c
@@ -0,0 +1,52 @@
+#include "search_algos.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * binary_search - matches value using binary search algorithm
+ * @array: array of integers
+ * @size: size of array
+ * @value: value to match
+ *
+ * Return: index of match, otherwise -1
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	int idx;
+	int min = 0;
+	int max = size - 1;
+	int middle;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	while (min <= max)
+	{
+		printf("Searching in array: ");
+		for (idx = min; idx <= max; idx++)
+		{
+			if (idx == max)
+				printf("%i\n", array[idx]);
+			else
+				printf("%i, ", array[idx]);
+		}
+
+		middle = (min + max) / 2;
+		if (value == array[middle])
+		{
+			return (middle);
+		}
+		else if (value < array[middle])
+		{
+			max = middle - 1;
+		}
+		else if (value > array[middle])
+		{
+			min = middle + 1;
+		}
+	}
+	    return (-1);
+}
