98e9d804b5a6544f6360fa58bcf45fb4667d5714
Andy <3775@holbertonschool.com>
2022-08-31 13:38:18 -0400

    Try David's

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,82 +1,52 @@
 #include "search_algos.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * binary_search - searches for a value in array using binary search
- * @array: pointer to int array
- * @size: size_t size of array
- * @value: int value to search for
+ * binary_search - matches value using binary search algorithm
+ * @array: array of integers
+ * @size: size of array
+ * @value: value to match
  *
- * Return: int index where value was found or -1 if not found
+ * Return: index of match, otherwise -1
  */
+
 int binary_search(int *array, size_t size, int value)
 {
+	int idx;
+	int min = 0;
+	int max = size - 1;
+	int middle;
 
-	int low_idx, high_idx;
-
-	low_idx = 0;
-	high_idx = size - 1;
-
-	return (rec_bin_search(array, low_idx, high_idx, value));
-
-}
-
-
-/**
- * rec_bin_search - searches for a value in a sorted array
- * @array: pointer to int array
- * @low_idx: int first index of sub-array
- * @high_idx: int last index of sub-array
- * @value: int value we are searching for
- *
- * Return: int index where value is found or -1 if not found
- */
-int rec_bin_search(int *array, int low_idx, int high_idx, int value)
-{
-
-	int mid_idx;
-
-	mid_idx = low_idx + (high_idx - low_idx) / 2;
-	if (low_idx > high_idx)
-		return (-1);
-
-	print_array(array, low_idx, high_idx);
-
-	if (value == array[mid_idx])
-		return (mid_idx);
-	else if (value < array[mid_idx])
-	{
-		return (rec_bin_search(array, low_idx, mid_idx - 1, value));
-	}
-	else if (value > array[mid_idx])
-	{
-		return (rec_bin_search(array, mid_idx + 1, high_idx, value));
-	}
-	else
+	if (array == NULL)
 	{
 		return (-1);
 	}
-}
 
-
-/**
- * print_array - print formatted array
- * @array: pointer to int array
- * @low_idx: int first index of sub-array
- * @high_idx: int last index of sub-array
- *
- * Return: no return value
- */
-void print_array(int *array, int low_idx, int high_idx)
-{
-	int i;
-
-	printf("Searching in array: ");
-
-	for (i = low_idx; i < high_idx; i++)
+	while (min <= max)
 	{
-		printf("%i, ", array[i]);
-	}
-	printf("%i", array[high_idx]);
+		printf("Searching in array: ");
+		for (idx = min; idx <= max; idx++)
+		{
+			if (idx == max)
+				printf("%i\n", array[idx]);
+			else
+				printf("%i, ", array[idx]);
+		}
 
-	printf("\n");
+		middle = (min + max) / 2;
+		if (value == array[middle])
+		{
+			return (middle);
+		}
+		else if (value < array[middle])
+		{
+			max = middle - 1;
+		}
+		else if (value > array[middle])
+		{
+			min = middle + 1;
+		}
+	}
+	    return (-1);
 }
