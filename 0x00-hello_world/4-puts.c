b992627fa870af2e11ee6cbbcc6df4b7cdce34d1
andy <astonemn@gmail.com>
2025-07-16 14:18:06 -0500

    Add function description and alter output text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - prints a line to standard output
+ *
+ * Return: 0
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
