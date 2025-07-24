d3fd7e75024118e6ffb8311d77f265af97815393
andy <astonemn@gmail.com>
2025-07-23 20:18:36 -0500

    Initial commit task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - print all combinations of 2 single digits without repeating
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int c1 = 48;
+	int c2 = c1 + 1;
+	int c3 = c2 + 1;
+
+	while (c1 <= '9' || c2 <= '9' || c3 <= '9')
+	{
+		while (c2 <= '9' || c3 <= '9')
+		{
+			while (c3 <= '9')
+			{
+				putchar(c1);
+				putchar(c2);
+				putchar(c3);
+
+				if (!(c1 == '8' && c2 == '9' && c3 == '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				c3++;
+			}
+			c2++;
+		}
+		c1++;
+		c2 = c1 + 1;
+		c3 = c2 + 1;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
