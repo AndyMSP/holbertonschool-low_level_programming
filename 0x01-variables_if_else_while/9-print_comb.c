e9e1bfd78e13f4241ecccb1f394b286aab11709d
andy <astonemn@gmail.com>
2025-10-24 12:48:51 -0500

    Task 9 initial commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print 0123456789 to stdout
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+
+	return (0);
+}
