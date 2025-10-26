d039fbcec317ee14ba937ed58df98a0e202e0a7b
andy <astonemn@gmail.com>
2025-10-25 21:21:58 -0500

    Task 0 initial commit

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+
+/**
+ * main - Generates a random number and determines its sign
+ *
+ * Return: Always 0
+ */
+void positive_or_negative(int n)
+{
+
+	/* 	srand(time(0));
+		n = rand() - RAND_MAX / 2;
+		your code goes there */
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("0 is zero\n");
+	/* your code ends here*/
+}
