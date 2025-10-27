c05c97aa60248a9e1df13ad6724ad3cba177bbb6
andy <astonemn@gmail.com>
2025-10-26 21:26:50 -0500

    Task 10 initial commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - Print a triangle in the terminal
+ * @size: integer height of triangle
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i, j, spaces, hashes;
+
+	for (i = 1; i <= size; i++)
+	{
+		spaces = size - i;
+		hashes = size - spaces;
+
+		for (j = 0; j < spaces; j++)
+		{
+			_putchar('.');
+		}
+		for (j = 0; j < hashes; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (size == 0)
+		_putchar('\n');
+}
