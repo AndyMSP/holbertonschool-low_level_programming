9da0b2726cd6c289fc7be620d0056d594c158eb5
andy <astonemn@gmail.com>
2025-11-03 15:51:36 -0600

    Task 8 initial commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print an array of integers
+ * @a: pointer to int
+ * @n: number of integers to print
+ *
+ * Return: Void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	i++;
+	printf("%d\n", a[i]);
+}
