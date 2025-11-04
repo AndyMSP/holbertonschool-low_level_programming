aa73eec29b8295b671d8db4ca24f260ea062839b
andy <astonemn@gmail.com>
2025-11-04 17:30:36 -0600

    Task 5 initial commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - convert any lowercase characters to uppercase
+ * @str: string to process
+ *
+ * Return: str
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\n'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = 'A' + str[i] - 'a';
+		}
+	}
+
+	return (str);
+}
