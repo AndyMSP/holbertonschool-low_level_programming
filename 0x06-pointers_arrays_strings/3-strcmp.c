414d9cc22b7ad2e55cc1ac56c843bf0ceb640d5f
andy <astonemn@gmail.com>
2025-11-03 20:45:38 -0600

    Task 3 initial commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcmp - compare two strings
+ * @s1: first string for comparison
+ * @s2: second string to compare
+ *
+ * Return: int - positive if s1>s2, 0 if equal and negative if s1<s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 || *s2)
+	{
+		if (!(*s1))
+			return (-1 * *s2);
+		else if (!(s1))
+			return (*s1);
+		else if (s1 != s2)
+			return (*s1 - *s2);
+		else
+		{
+			s1++;
+			s2++;
+		}
+	}
+
+	return (0);
+}
