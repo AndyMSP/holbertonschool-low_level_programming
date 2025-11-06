fbc32563b0edec2b879c9cbd2c50fb162a17e2bb
andy <astonemn@gmail.com>
2025-11-06 15:49:37 -0600

    Task 2 initial commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strchr - Find a character in a string
+ * @s: string to search
+ * @c: character to search for
+ *
+ * Return: Pointer to first character found or null
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (0);
+}
