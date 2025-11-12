64931e0592d9a92dc7213c5c1f7ce29a3a12b60f
andy <astonemn@gmail.com>
2025-11-11 21:11:32 -0600

    Task 2 initial commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - Find length of string recursively
+ * @s: string to examine
+ *
+ * Return: int length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (!*s)
+	{
+		return (0);
+	}
+
+	s++;
+	len = _strlen_recursion(s) + 1;
+	return (len);
+}
