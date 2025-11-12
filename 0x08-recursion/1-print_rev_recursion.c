2e20b9fd8bf2380f6f6eb1d9bf9193b5c681cbab
andy <astonemn@gmail.com>
2025-11-11 20:19:07 -0600

    Task 1 initial commit

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print a string in reverse using recursion
+ * @s: string to print in reverse
+ *
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (!*s)
+	{
+		return;
+	}
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
