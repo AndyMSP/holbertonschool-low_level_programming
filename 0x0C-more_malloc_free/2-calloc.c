47dc2b49eda2a3d95c6804e5ea2263855375f452
andy <astonemn@gmail.com>
2025-11-17 17:20:27 -0600

    Initial commit

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - allocate and initialize memory (all to zero)
+ * @nmemb: number of members to allocate
+ * @size: size in bytes of each member
+ *
+ * Return: pointer to new memory or null on failure
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *mem = NULL;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	mem = malloc(nmemb * size);
+	if (!mem)
+		return (NULL);
+
+	return (mem);
+}
