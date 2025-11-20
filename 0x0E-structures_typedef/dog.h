f883d10aa97e91a947546a3cecbf41ae8073c289
andy <astonemn@gmail.com>
2025-11-19 23:09:51 -0600

    Initial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - data representing a dog
+ * @name: char * dogs name
+ * @age: float age of dog
+ * @owner: char * owner's name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
