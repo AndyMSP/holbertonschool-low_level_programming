1933a5ea568d48a2a668986ff8d4699c67088b81
andy <astonemn@gmail.com>
2025-11-20 15:28:20 -0600

    Initial commit

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - free memory of dog
+ * @d: pointer to a dog element
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
