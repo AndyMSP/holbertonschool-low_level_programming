84609670508acefc43e4c85a3f45657ccae920d4
andy <astonemn@gmail.com>
2025-12-12 19:49:22 -0600

    Initial commit

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include "stdlib.h"
+
+/**
+ * free_list - free all allocated memory from linked list
+ * @head: pointer to first node
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *node, *next;
+
+	node = head;
+
+	while (node->next)
+	{
+		next = node->next;
+		free(node->str);
+		free(node);
+		node = next;
+	}
+	free(node->str);
+	free(node);
+}
--- 0x12-singly_linked_lists/4-main.c
+++ 0x12-singly_linked_lists/4-main.c
@@ -13,10 +13,13 @@ int main(void)
 	list_t *head;
 
 	head = NULL;
-
-	add_node(&head, "one");
-	add_node(&head, "two");
-	add_node(&head, "three");
+	/* add_node_end(&head, "Bob");
+	add_node_end(&head, "&");
+	add_node_end(&head, "Kris");
+	add_node_end(&head, "love");
+	add_node_end(&head, "asm"); */
 	print_list(head);
+	free_list(head);
+	head = NULL;
 	return (0);
 }
